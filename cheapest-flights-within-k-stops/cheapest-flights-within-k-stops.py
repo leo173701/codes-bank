7453d8b31982b72c5b6054dff6bc6547474bb162
Bo Lei <leibo2014@yeah.net>
2021-11-03 13:14:25 -0400

    Time: 104 ms (83.09%), Space: 15.1 MB (87.78%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,18 +1,23 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        visited = {}
+        # dic = {}
         graph = collections.defaultdict(list)
-        for a, b, c in flights:
-            graph[a].append((b, c))
-
-        queue = deque([(src, 0, 0)])
-        res = [float('inf')] * n
-        res[src] = 0
-        while queue:
-            curr, cost, cnt = queue.popleft()
-            if cnt>=k+1:
-                break
-            for temp, c in graph[curr]:
-                if res[temp]>cost+c:
-                    res[temp] = cost+c 
-                    queue.append((temp, cost+c, cnt+1)) 
-        return res[dst] if res[dst]!=float('inf') else -1
+        for s, d, p in flights:
+            graph[s].append((d, p))
+        # for start, end, price in flights:
+        #     if start not in dic:
+        #         dic[start] = [(end,price)]
+        #     else:
+        #         dic[start].append((end,price))
+        heap = [(0,0,src)]
+        while heap:
+            # n = len(heap)
+            totalprice, moves, node = heapq.heappop(heap)
+            if node ==dst and k>=moves-1:
+                return totalprice
+            if node not in visited or visited[node]>moves:
+                visited[node]=moves
+                for nextnode, price in graph[node]:
+                    heapq.heappush(heap,(totalprice + price, moves+1, nextnode))
+        return -1
