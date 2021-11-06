c3dd72f5bdbbd3a025ebc3eb52aa3f201823524e
Bo Lei <leibo2014@yeah.net>
2021-11-06 10:08:58 -0400

    Time: 187 ms (65.75%), Space: 14.3 MB (98.81%) - LeetHub

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,40 +1,39 @@
-DIR = ((-1,-1), (-1, 0),(-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
-        # DIR = ((-1,-1), (-1, 0),(-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-        x0, y0 = click[0], click[1]
-        board = board.copy()
-        
-        if board[x0][y0] == 'M':
-            board[x0][y0] = 'X'
+        if board[click[0]][click[1]]=="M":
+            board[click[0]][click[1]]="X"
             return board
+        m = len(board)
+        n = len(board[0])
+        queue = collections.deque([click])
+        directions = [(-1,0),(1,0),(0,1),(0,-1),(-1,1),(1,1),(-1,-1),(1,-1)]  
+        # digits = {"B","1","2","3","4","5","6","7","8"}
+        def count(curx,cury):
+            count_mine = 0
+            for dx, dy in directions:
+                newx, newy = curx +dx,cury+dy
+                if 0<=newx<m and 0<=newy<n:   #判断基本的还在边界范围内
+                    if board[newx][newy]=="M":
+                        count_mine+=1         # 在四周有多少个mine
+            return count_mine
         
-        cnt = self.cnt(board, x0, y0)
-        if cnt != 0:
-            board[x0][y0] = str(cnt)
+        count_mine = count(click[0], click[1])
+        if count_mine>0:
+            board[click[0]][click[1]]=str(count_mine)
             return board
-        
-        board[x0][y0] = 'B'
-        m, n = len(board), len(board[0])
-        q = deque([(click[0], click[1])])
-        while q:
-            x, y = q.popleft()
-            for dx, dy in DIR:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx <= m - 1 and 0 <= ny <= n - 1 and board[nx][ny] == 'E':
-                    cnt = self.cnt(board, nx, ny)
-                    if cnt != 0:
-                        board[nx][ny] = str(cnt)
+        else:
+            board[click[0]][click[1]]="B"
+        while queue:
+            curx,cury = queue.popleft()
+            for dx, dy in directions:
+                newx, newy = curx +dx,cury+dy
+                if 0<=newx<m and 0<=newy<n and board[newx][newy]=="E":   #判断基本的还在边界范围内
+                    # if board[newx][newy] in digits:
+                    #     continue              # 去除重复
+                    count_mine = count(newx, newy)
+                    if count_mine>0:
+                        board[newx][newy]=str(count_mine)
                     else:
-                        board[nx][ny] = 'B'
-                        q.append((nx, ny))
+                        board[newx][newy]="B"
+                        queue.append((newx,newy))
         return board
-                        
-    def cnt(self, board, x, y):
-        m, n = len(board), len(board[0])
-        res = 0
-        for dx, dy in DIR:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx <= m - 1 and 0 <= ny <= n - 1 and board[nx][ny] == 'M':
-                res += 1
-        return res
