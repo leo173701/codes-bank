e8f7bb3304164e81551327bbb4b1bff33f1832e0
Bo Lei <leibo2014@yeah.net>
2021-11-06 10:14:17 -0400

    Time: 192 ms (56.13%), Space: 14.5 MB (96.97%) - LeetHub

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,39 +1,33 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
-        if board[click[0]][click[1]]=="M":
-            board[click[0]][click[1]]="X"
-            return board
-        m = len(board)
-        n = len(board[0])
-        queue = collections.deque([click])
-        directions = [(-1,0),(1,0),(0,1),(0,-1),(-1,1),(1,1),(-1,-1),(1,-1)]  
-        # digits = {"B","1","2","3","4","5","6","7","8"}
-        def count(curx,cury):
-            count_mine = 0
-            for dx, dy in directions:
-                newx, newy = curx +dx,cury+dy
-                if 0<=newx<m and 0<=newy<n:   #判断基本的还在边界范围内
-                    if board[newx][newy]=="M":
-                        count_mine+=1         # 在四周有多少个mine
-            return count_mine
+    # def updateBoard(self, board, click):
+        """
+        :type board: List[List[str]]
+        :type click: List[int]
+        :rtype: List[List[str]]
+        """
+        R, C = len(board), len(board[0])
+        if board[click[0]][click[1]] == "M": board[click[0]][click[1]] = "X"
+        dir = [1,0], [0,1], [-1,0],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]
+        q = collections.deque()
+        q.append(click)
+        seen = set(click)
         
-        count_mine = count(click[0], click[1])
-        if count_mine>0:
-            board[click[0]][click[1]]=str(count_mine)
-            return board
-        else:
-            board[click[0]][click[1]]="B"
-        while queue:
-            curx,cury = queue.popleft()
-            for dx, dy in directions:
-                newx, newy = curx +dx,cury+dy
-                if 0<=newx<m and 0<=newy<n and board[newx][newy]=="E":   #判断基本的还在边界范围内
-                    # if board[newx][newy] in digits:
-                    #     continue              # 去除重复
-                    count_mine = count(newx, newy)
-                    if count_mine>0:
-                        board[newx][newy]=str(count_mine)
-                    else:
-                        board[newx][newy]="B"
-                        queue.append((newx,newy))
-        return board
+        def numBombsTangent(board, i, j):
+            count = 0 
+            for x, y in dir: 
+                if 0 <= i + x < R and 0 <= j + y < C and board[i+x][y+j] == "M": count += 1 
+            return count 
+
+        while q: 
+            for tup in range(len(q)):
+                x, y = q.popleft()
+                if board[x][y] == "E":
+                    bombsNextTo = numBombsTangent(board, x, y)
+                    board[x][y] = "B" if bombsNextTo == 0 else str(bombsNextTo)
+                    if bombsNextTo == 0:
+                        for a, b in dir:
+                            if 0 <= a + x < R and 0 <= b + y < C and (a+x,b+y) not in seen:
+                                q.append((a+x, b+y))
+                                seen.add((a+x, b+y))
+        return board 
