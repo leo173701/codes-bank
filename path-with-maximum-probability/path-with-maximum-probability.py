0a2d184e7a8740cd10b5323b5675fb8e3836ccfa
Bo Lei <leibo2014@yeah.net>
2021-11-09 16:47:26 -0500

    Time: 788 ms (43.88%), Space: 27.4 MB (12.64%) - LeetHub

--- path-with-maximum-probability/path-with-maximum-probability.py
+++ path-with-maximum-probability/path-with-maximum-probability.py
@@ -1,26 +1,27 @@
-from collections import defaultdict
+# from collections import defaultdict
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        p = [0 for _ in range(n)]
+        p = [0]*n
         p[start]=1
         probability = {}
         for i in range(len(edges)):
             probability[(edges[i][0],edges[i][1])]=succProb[i]
             probability[(edges[i][1],edges[i][0])]=succProb[i]
-        pq = ([])
-        heapq.heappush(pq, start)
-        # visited ={start}
+        pq = deque([start])
+        
+        # heapq.heappush(pq, start)
         graph =collections.defaultdict(list)
         for i in range(len(edges)):
             graph[edges[i][0]].append(edges[i][1])
             graph[edges[i][1]].append(edges[i][0])
-        # print(graph)
-        # print(probability)
+
         while pq:
-            cur = heapq.heappop(pq)
+            cur = pq.popleft()
+            # cur = heapq.heappop(pq)
             for nextnode in graph[cur]:
                 temp = p[cur]*probability[(cur,nextnode)]
                 if p[nextnode]<temp:
                     p[nextnode]=temp
-                    heapq.heappush(pq,nextnode)
+                    pq.append(nextnode)
+                    # heapq.heappush(pq,nextnode)
         return float(p[end])
