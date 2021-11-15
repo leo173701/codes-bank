5aa31136f816fe0f1b2bec2c54c890bb09213f8c
Bo Lei <leibo2014@yeah.net>
2021-11-15 11:16:50 -0500

    Time: 100 ms (90.35%), Space: 15.2 MB (68.91%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,23 +1,24 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        visited = {}
-        # dic = {}
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
         graph = collections.defaultdict(list)
-        for s, d, p in flights:
-            graph[s].append((d, p))
-        # for start, end, price in flights:
-        #     if start not in dic:
-        #         dic[start] = [(end,price)]
-        #     else:
-        #         dic[start].append((end,price))
-        heap = [(0,0,src)]
-        while heap:
-            # n = len(heap)
-            totalprice, moves, node = heapq.heappop(heap)
-            if node ==dst and k>=moves-1:
-                return totalprice
-            if node not in visited or visited[node]>moves:
-                visited[node]=moves
-                for nextnode, price in graph[node]:
-                    heapq.heappush(heap,(totalprice + price, moves+1, nextnode))
-        return -1
+        for flight in flights:
+            graph[flight[0]].append(flight[1:])
+        distance = [sys.maxsize] * n
+        distance[src] = 0
+        queue = collections.deque([(src, 0)]) # 
+        while queue:
+            tmp_dst = list(distance) # nextlevel
+            size = len(queue)
+            for _ in range(size):
+                u, steps = queue.popleft()
+                if steps > K:
+                    continue
+                for v, cost in graph[u]:
+                    if cost + distance[u] < tmp_dst[v]:
+                        tmp_dst[v] = distance[u] + cost
+                        queue.append((v, steps + 1))
+            distance = tmp_dst
+        if distance[dst] != sys.maxsize:
+            return distance[dst]
+        else:
+            return -1
