1b201b5d7fc3380a371cf9292d0651aa5df9e39f
Bo Lei <leibo2014@yeah.net>
2021-11-15 11:23:09 -0500

    Time: 296 ms (22.96%), Space: 15.3 MB (47.06%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,24 +1,13 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        graph = collections.defaultdict(list)
-        for flight in flights:
-            graph[flight[0]].append(flight[1:])
-        distance = [sys.maxsize] * n
-        distance[src] = 0
-        queue = collections.deque([(src, 0)]) # 
-        while queue:
-            tmp_dst = list(distance) # nextlevel
-            size = len(queue)
-            for _ in range(size):
-                u, steps = queue.popleft()
-                if steps > K:
-                    continue
-                for v, cost in graph[u]:
-                    if cost + distance[u] < tmp_dst[v]:
-                        tmp_dst[v] = distance[u] + cost
-                        queue.append((v, steps + 1))
-            distance = tmp_dst
-        if distance[dst] != sys.maxsize:
-            return distance[dst]
-        else:
-            return -1
+        kInfCost = float('inf')
+        cost = [kInfCost for _ in range(n)]
+        cost[src] = 0
+
+        for i in range(K + 1):
+            tmp = list(cost)
+            for p in flights:
+                tmp[p[1]] = min(tmp[p[1]], cost[p[0]] + p[2])
+            cost = tmp
+
+        return -1 if cost[dst] >= kInfCost else cost[dst]
