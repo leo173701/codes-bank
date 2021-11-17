4a7af5f916ef01601e91a87f61c002b62f30ec1c
Bo Lei <leibo2014@yeah.net>
2021-11-17 12:47:27 -0500

    Time: 40 ms (73.46%), Space: 14.4 MB (71.89%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,16 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
+        visited = [False for _ in range(n)]
         res = []
-        def dfs(nums, track, res):
-            if len(track)==n:
-                res.append(track[:])
-            for i in nums:
-                if i in track:
+        def dfs(nums, n, path):
+            if len(path)==n:
+                res.append(path[:])
+            for index,value in enumerate(nums) :
+                if visited[index]==True:
                     continue
-                track.append(i)
-                dfs(nums,track,res)
-                track.pop()
-        dfs(nums,[],res)
-        
-        return res 
+                visited[index]=True
+                path.append(value)
+                dfs(nums,n,path)
+                path.pop()
+                visited[index]=False
+        dfs(nums,n,[])
+        return res
