4ff3fea25be396e88bea6357800ba682c19c2568
Bo Lei <leibo2014@yeah.net>
2021-11-17 12:40:52 -0500

    Time: 60 ms (93.06%), Space: 14.5 MB (35.06%) - LeetHub

---
+++ permutation-in-string/permutation-in-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        m = len(s1)
+        if len(s2)<m:
+            return False
+        dic1 = {}
+        dic2 = {}
+        for i in range(m):
+            dic2[s2[i]] = dic2.get(s2[i],0)+1 
+            dic1[s1[i]] = dic1.get(s1[i],0)+1 
+        if dic1 == dic2:
+            return True
+        for i in range(m,len(s2)):
+            
+            dic2[s2[i]] = dic2.get(s2[i],0)+1  
+            dic2[s2[i-m]] -=1
+            
+            if dic2[s2[i-m]]==0:
+                del dic2[s2[i-m]]
+            if dic1==dic2:
+                return True
+        return False
