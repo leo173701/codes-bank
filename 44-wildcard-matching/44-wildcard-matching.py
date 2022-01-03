f12a0cd510038023ba43d769b51eb86609f1b73c
Bo Lei <leibo2014@yeah.net>
2022-01-03 12:57:58 -0500

    Time: 947 ms (39.57%), Space: 22.4 MB (67.83%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -7,9 +7,10 @@ class Solution:
                 dp[0][i] = dp[0][i - 1]
         for i in range(1, len(s) + 1):
             for j in range(1, len(p) + 1):
-                if p[j-1]!="*":
-                    if s[i-1]==p[j-1] or p[j-1]=="?":
-                        dp[i][j]=dp[i-1][j-1]
-                else:
+                if p[j-1]=="*":
                     dp[i][j]=dp[i][j-1] or dp[i-1][j]  #"abcd"   p="ab*cd" *对应空序列，当做不存在
+                else:
+                    if dp[i-1][j-1]:
+                        if s[i-1]==p[j-1] or p[j-1]=="?":
+                            dp[i][j]=dp[i-1][j-1]
         return dp[-1][-1]
