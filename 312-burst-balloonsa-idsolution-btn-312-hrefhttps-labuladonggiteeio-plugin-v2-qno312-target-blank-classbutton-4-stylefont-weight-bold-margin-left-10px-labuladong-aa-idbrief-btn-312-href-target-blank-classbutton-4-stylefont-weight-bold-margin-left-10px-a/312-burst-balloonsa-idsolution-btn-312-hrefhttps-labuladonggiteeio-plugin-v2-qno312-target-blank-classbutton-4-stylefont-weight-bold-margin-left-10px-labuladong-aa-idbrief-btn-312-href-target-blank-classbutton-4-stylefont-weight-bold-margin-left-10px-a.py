bffbcd9ac485dced58363caefd670589b2e4d4d4
Bo Lei <leibo2014@yeah.net>
2022-01-09 14:23:23 -0500

    Time: 7892 ms (52.42%), Space: 19.9 MB (42.17%) - LeetHub

--- 312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.py
+++ 312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.py
@@ -7,6 +7,7 @@ class Solution:
         dp=[[0]*n  for _ in range(n)]
         for i in range(n-1,-1,-1):
             for j in range(i+2,n):
+                temp_ij=nums[i]*nums[j]
                 for k in range(i+1,j):
-                    dp[i][j]=max(dp[i][j], dp[i][k]+dp[k][j]+nums[i]*nums[j]*nums[k])
+                    dp[i][j]=max(dp[i][j], dp[i][k]+dp[k][j]+temp_ij*nums[k])
         return dp[0][n-1]
