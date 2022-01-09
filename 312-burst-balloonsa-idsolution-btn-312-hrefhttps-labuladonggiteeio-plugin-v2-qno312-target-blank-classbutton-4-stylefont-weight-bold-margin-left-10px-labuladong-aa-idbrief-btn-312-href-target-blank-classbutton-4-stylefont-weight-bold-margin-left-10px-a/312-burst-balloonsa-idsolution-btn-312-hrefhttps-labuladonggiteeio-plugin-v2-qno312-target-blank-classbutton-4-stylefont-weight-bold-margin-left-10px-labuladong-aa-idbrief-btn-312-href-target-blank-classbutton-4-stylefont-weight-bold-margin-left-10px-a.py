4fc225e6af5682e0f3098a4534a979edf18fdbad
Bo Lei <leibo2014@yeah.net>
2022-01-09 14:16:05 -0500

    Time: 12214 ms (17.97%), Space: 19.9 MB (56.80%) - LeetHub

---
+++ 312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/312-burst-balloonsa-idsolution-btn-312-hrefhttps-labuladonggiteeio-plugin-v2-qno312-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-312-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp=[[0]*n  for _ in range(n)]
+        for i in range(n-1,-1,-1):
+            for j in range(i+2,n):
+                for k in range(i+1,j):
+                    dp[i][j]=max(dp[i][j], dp[i][k]+dp[k][j]+nums[i]*nums[j]*nums[k])
+        return dp[0][n-1]
